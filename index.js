40351435f96eb56a59da66ce0c61f4704c5b0ff0
NunesNathan <nunesxd7@hotmail.com>
2022-05-05 20:42:09 -0300

    :art: (refactor) - Setup index app

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./routers');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(bodyParser.json());
+app.use(router);
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
